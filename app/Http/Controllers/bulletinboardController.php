d41599bd5d89d6a63fcf59f8b370bdd1a36f40bd
amano <amano-h@cyport.jp>
2020-08-27 09:52:10 +0900

    2020/8/27 天野仁美

--- app/Http/Controllers/bulletinboardController.php
+++ app/Http/Controllers/bulletinboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\thread;
 use App\comment;
+use Illuminate\Support\Facades\DB;
 
 class bulletinboardController extends Controller
 {
@@ -24,14 +25,32 @@ class bulletinboardController extends Controller
     }
     
     //
-    public function contents(){
+    public function contents(Request $request){
   
         $data['threads']=  thread:: all() ;
         $data['comments']= comment:: all() ;
-
+        $thread_id = $request->thread_id;
+	    $data['threads'] = DB::table('thread')->where('thread_id','=',$thread_id)->get();
+	    $comment_id = $request->comment_id;
+	    $data['comments'] = DB::table('comment')->where('thread_id','=',$thread_id)->get();
+	    
     	return view('bulletinboard.contents',$data);
     }
     
+    //
+    public function create(Request $request) {
+	
+	$param = [
+	'name' => $request->name,
+	'message' => $request->message,
+	'created_at' =>$request->now(),
+	];
+	
+	DB::table('thread')->insert($param);
+		
+		return redirect('bulletinboard');
+	}
+
     
 }
     
