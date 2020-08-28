66e1791f1ca16072784d0ce07c09d1684bdfc7a9
amano <amano-h@cyport.jp>
2020-08-28 17:20:14 +0900

    2020/8/28

--- app/Http/Controllers/bulletinboardController.php
+++ app/Http/Controllers/bulletinboardController.php
@@ -9,49 +9,59 @@ use Illuminate\Support\Facades\DB;
 
 class bulletinboardController extends Controller
 {
-    //
-    public function list(){
-    
-    	$data=['threads' => thread:: all() ];
-    
-    	return view('bulletinboard.list',$data);
+    //リスト
+    public function list()
+    {
+        $data['threads']=  thread:: all() ;
+        $data['comments']= comment:: all() ;
+
+        return view('bulletinboard.list',$data);
     }
-    //
-    public function register(Request $request){
-  
-    
- 
-    	return view('bulletinboard.register');
+
+    //レジスター
+    public function register(Request $request)
+    {
+        $data['threads']=  thread:: all() ;
+        $data['comments']= comment:: all() ;
+
+        return view('bulletinboard.register');
     }
-    
-    //
-    public function contents(Request $request){
-  
+
+    //コンテンツ
+    public function contents(Request $request)
+    {
         $data['threads']=  thread:: all() ;
         $data['comments']= comment:: all() ;
         $thread_id = $request->thread_id;
-	    $data['threads'] = DB::table('thread')->where('thread_id','=',$thread_id)->get();
-	    $comment_id = $request->comment_id;
-	    $data['comments'] = DB::table('comment')->where('thread_id','=',$thread_id)->get();
-	    
-    	return view('bulletinboard.contents',$data);
+        $data['threads'] = DB::table('thread')->where('thread_id','=',$thread_id)->get();
+        $comment_id = $request->comment_id;
+        $data['comments'] = DB::table('comment')->where('thread_id','=',$thread_id)->get();
+
+        return view('bulletinboard.contents',$data);
     }
-    
-    //
-    public function create(Request $request){
-	
-	$param = [
-	'thread_id' => $request->thread_id,
-	'name' => $request->name,
-	'message' => $request->message,
-	'delete_flag' =>$request->delete_flag,
-	'created_at' =>$request->now(),
-	];
-	
-	DB::table('thread')->insert($param);
-		
-		return redirect('bulletinboard');
-	}
-	
+
+    //レコード追加
+    public function create(Request $request)
+    {
+        $param = [
+        'thread_id' => $request->thread_id,
+        'name' => $request->name,
+        'message' => $request->message,
+        'delete_flag' =>$request->delete_flag,
+        'created_at' =>$request->now(),];
+
+         DB::table('thread')->insert($param);
+
+        return redirect('bulletinboard');
+    }
+
+    //スレッド削除
+    public function delete(Request $request)
+    {
+        $item = DB::table('thread')
+             ->where('thread_id',$request->thread_id)->first();
+        return view ('bulletinboard/list',['form' => $item]);
+    }
+
 }
     
