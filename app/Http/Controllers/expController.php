ad55a0342dea4b111b520b60a0301f2153ea76f6
arima <arima@cyport.jp>
2020-08-31 17:11:42 +0900

    スタッフ経験関連、不要なメソッド削除

--- app/Http/Controllers/expController.php
+++ app/Http/Controllers/expController.php
@@ -6,22 +6,23 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
-class expController extends Controller
+class expcontroller extends Controller
 {
 	public function meun() {
-	$items = DB::table('test')->get();
+	$items = DB::table('staff_experience')->get();
 	return view ('exp.expmeun',['items' => $items]);
 	}
+	
 	public function edit(Request $request) {
-
-	$item = DB::table('exp')
+	$item = DB::table('staff_experience')
 	->where('s_id',$request->s_id)->first();
 	return view ('exp.expedit',['form' => $item]);
 	}
+	
 	public function post(Request $request) {
-
 	$param = [
 	's_id' => $request->s_id,
+	'name' => $request->name,
 	'industry' => $request->industry,
 	'phase' => $request->phase,
 	'pro_name' => $request->pro_name,
@@ -31,15 +32,45 @@ class expController extends Controller
 	'end_period' => $request->end_period,
 	'content' => $request->content,
 	'result' => $request->result,
+	'mod_os' => $request->mod_os,
+	'DB_DC' => $request->DB_DC,
+	'lang_tool' => $request->lang_tool,
 	];
-	DB::table('exp')
+	DB::table('staff_experience')
 		->where('s_id',$request->s_id)
 		->update($param);
 		return redirect('expmeun');
 	}
+	
 	public function view(Request $request) {
 	$s_id = $request->s_id;
-	$items = DB::table('exp')->where('s_id','=',$s_id)->get();
+	$items = DB::table('staff_experience')->where('s_id','=',$s_id)->get();
 	return view ('exp.expview',['items' => $items]);
+	}
+	
+	public function reg(Request $request) {	
+	return view ('exp.expreg');
+	}
+	
+	public function regpost(Request $request) {
+	$param = [
+	's_id' => $request->s_id,
+	'name' => $request->name,
+	'industry' => $request->industry,
+	'phase' => $request->phase,
+	'pro_name' => $request->pro_name,
+	'pro_content' => $request->pro_content,
+	'notices' => $request->notices,
+	'start_period' => $request->start_period,
+	'end_period' => $request->end_period,
+	'content' => $request->content,
+	'result' => $request->result,
+	'mod_os' => $request->mod_os,
+	'DB_DC' => $request->DB_DC,
+	'lang_tool' => $request->lang_tool,
+	];
+	DB::table('staff_experience')
+		->insert($param);
+		return redirect('expmeun');
 	}
 }
