dd1f6f97c4da736e02414d24b1fd204271dda240
amano <amano-h@cyport.jp>
2020-08-31 17:48:00 +0900

    2020/8/31 天野

--- app/Http/Controllers/bulletinboard/createController.php
+++ app/Http/Controllers/bulletinboard/createController.php
@@ -14,13 +14,13 @@ class createController extends Controller
     //ƒŒƒR[ƒh’Ç‰Á
     public function create(Request $request)
     {
+        \Log::info('“Še‰æ–ÊStart');
         $param = new thread();
         \Log::info($request);
         $param -> name = $request->name;
         $param -> message = $request->message;
         $param -> delete_flag = 0;
-        $param -> read_id = $request->created_at;
-
+      
          DB::table('thread')->insert($param);
 
         return redirect('bulletinboard');
--- resources/views/bulletinboard/register.blade.php
+++ resources/views/bulletinboard/register.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.base')
 @section('main')
 
-<div class="col-sm-6">
-<div class="form-group">
-
 <form method="post" action="{{action('bulletinboard\createrController@create')}}">
 @csrf
 
+<div class="col-sm-6">
+<div class="form-group">
 <lavel for="title">■タイトル</label>
 <input type="text" class="form-control"  name="name" placeholder="(例)PHPについて">
 </div></div>
@@ -22,7 +21,7 @@
 <br>
 
 <button type="submit" class="btn btn-info">投稿</button>
-</a>
+
 
 </form>
 
